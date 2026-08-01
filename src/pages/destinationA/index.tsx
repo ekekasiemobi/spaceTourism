7a187e7f69c6bf71d2c5601be210323084e15426
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2026-08-01 01:05:08 +0100

    updated the destination and technology page

--- src/pages/destinationA/index.tsx
+++ src/pages/destinationA/index.tsx
@@ -1,60 +1,205 @@
+import { useState } from 'react'
 import Moon from '../../assets/image-moon.webp'
+// import Mars from '../../assets/destination/image-mars.webp'
+import Mars from '../../assets/image-mars.webp'
+import Titan from '../../assets/image-titan.png'
+import Europa from '../../assets/image-europa.png'
 import Nav from '../../components/nav'
 
+
 function DestinationA() {
+    const [active, setActive] = useState("MOON");
 
-  return (
+    const handleActive = (name: string) => {
+        setActive(name)
+    }
 
-    <>
+    return (
         <div className="min-h-screen destination relative">
 
             <Nav />
 
-            <div className='w-[80vw] mx-auto'>
-                <p className='text-[#A6ABCA] barlow pt-30 text-2xl' >01 PICK YOUR DESTINATION</p>
+       
+            <div className='container mx-auto mb-8'>
+                <p className='text-[#A6ABCA] barlow pt-30 text-2xl'>01 PICK YOUR DESTINATION</p>
             </div>
 
-            <div className="flex justify-between w-[80vw] mx-auto items-center pt-10">
-                <div className='w-[50vw]'>
-                    <img className='w-115' src={Moon} alt="" />
-                </div>
+        
+            <div className="flex justify-between container mx-auto items-center">
 
-                
-                <div className=' flex flex-col gap-7 w-[30vw]'>
-                    
-                    <ul className='flex gap-5'>
-                        <li><a className='text-[#A6ABCA] barlow' href="/destinationA">MOON</a></li>
-                        <li><a className='text-[#A6ABCA] barlow' href="/destinationB">MARS</a></li>
-                        <li><a className='text-[#A6ABCA] barlow' href="/destinationC">EUROPA</a></li>
-                        <li><a className='text-[#A6ABCA] barlow' href="/destinationD">TITIAN</a></li>
-                    </ul>
-                    
+              
+                {/* <div className='w-1/2'>
+                    <img className='w-full' src={Moon} alt={active} />
+                </div> */}
 
-                    <div className='flex flex-col gap-5'>
+               
+                <div className='flex flex-col gap-7'>
+
+                    <div className='flex justify-center gap-8 pb-2 ml-65'>
+              
+                        <button 
+                            onClick={() => handleActive("MOON")} 
+                            className={`pb-2 transition-all ${active === "MOON" ? "border-b-2 border-white text-white barlow" : "text-gray-500 barlow hover:text-gray-300"}`}
+                        >
+                            MOON
+                        </button>
+            
+                  
+                        <button 
+                            onClick={() => handleActive("MARS")} 
+                            className={`pb-2 transition-all ${active === "MARS" ? "border-b-2 border-white text-white barlow" : "text-gray-500 barlow hover:text-gray-300"}`}
+                        >
+                            MARS
+                        </button>
                         
-                        <h1 className="text-white text-8xl bellefair">MOON</h1>
-                        <p className="text-[#A6ABCA] barlow">See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</p>
+                        <button 
+                            onClick={() => handleActive("EUROPA")} 
+                            className={`pb-2 transition-all ${active === "EUROPA" ? "border-b-2 border-white text-white barlow" : "text-gray-500 barlow hover:text-gray-300"}`}
+                        >
+                            EUROPA
+                        </button>
+                      
+                        <button 
+                            onClick={() => handleActive("TITAN")} 
+                            className={`pb-2 transition-all ${active === "TITAN" ? "border-b-2 border-white text-white barlow" : "text-gray-500 barlow hover:text-gray-300"}`}
+                        >
+                            TITAN
+                        </button>
+                    
                     </div>
 
-                    <div className='flex justify-between'>
-                        <div className='flex flex-col gap-2'>
-                            <p className='text-[#A6ABCA] barlow'>AVG. DISTANCE</p>
-                            <p className='text-white barlow text-2xl'>384,400 km</p>
-                        </div>
+                  
+                    {active === "MOON" && (
+                        <>
+                            <div className='flex gap-20 justify-center items-center'>
 
-                        <div className='flex flex-col gap-2'>
-                            <p className='text-[#A6ABCA] barlow'>Est. travel time</p>
-                            <p className='text-white barlow text-2xl'>3 days</p>
-                        </div>
-                    </div>
+                                <div className='w-[35%]'>
+                                    <img className='w-full' src={Moon} alt={active} />
+                                </div>
+
+                                <div className='w-[40%]'>
+                           
+                                    <div className='flex flex-col gap-5'>
+                                        <h1 className="text-white text-8xl bellefair">MOON</h1>
+                                        <p className="text-[#A6ABCA] barlow">See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</p>
+                                    </div>
+
+                                    <div className='flex justify-between border-t border-slate-800 pt-6'>
+                                        <div className='flex flex-col gap-2'>
+                                            <p className='text-[#A6ABCA] barlow text-xs tracking-widest'>AVG. DISTANCE</p>
+                                            <p className='text-white barlow text-2xl'>384,400 km</p>
+                                        </div>
+
+                                        <div className='flex flex-col gap-2'>
+                                            <p className='text-[#A6ABCA] barlow text-xs tracking-widest'>EST. TRAVEL TIME</p>
+                                            <p className='text-white barlow text-2xl'>3 DAYS</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    )}
+
+                    {active === "MARS" && (
+                        <>
+                            <div className='flex gap-20 justify-center items-center'>
+
+                                <div className='w-[35%]'>
+                                    <img className='w-full' src={Mars} alt={active} />
+                                </div>
+
+                                <div className='w-[40%]'>
+                           
+                                    <div className='flex flex-col gap-5'>
+                                        <h1 className="text-white text-8xl bellefair">MARS</h1>
+                                        <p className="text-[#A6ABCA] barlow">Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!</p>
+                                    </div>
+
+                                    <div className='flex justify-between border-t border-slate-800 pt-6'>
+                                        <div className='flex flex-col gap-2'>
+                                            <p className='text-[#A6ABCA] barlow text-xs tracking-widest'>AVG. DISTANCE</p>
+                                            <p className='text-white barlow text-2xl'>225,400 km</p>
+                                        </div>
+
+                                        <div className='flex flex-col gap-2'>
+                                            <p className='text-[#A6ABCA] barlow text-xs tracking-widest'>EST. TRAVEL TIME</p>
+                                            <p className='text-white barlow text-2xl'>9 MONTHS</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    )}
+
+                    {active === "EUROPA" && (
+                        <>
+                            <div className='flex gap-20 justify-center items-center'>
+
+                                <div className='w-[35%]'>
+                                    <img className='w-full' src={Europa} alt={active} />
+                                </div>
+
+                                <div className='w-[40%]'>
+                           
+                                    <div className='flex flex-col gap-5'>
+                                        <h1 className="text-white text-8xl bellefair">EUROPA</h1>
+                                        <p className="text-[#A6ABCA] barlow">The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. Take a dip in its icy ocean, or go ice-skating across its vast frozen surface.</p>
+                                    </div>
+
+                                    <div className='flex justify-between border-t border-slate-800 pt-6'>
+                                        <div className='flex flex-col gap-2'>
+                                            <p className='text-[#A6ABCA] barlow text-xs tracking-widest'>AVG. DISTANCE</p>
+                                            <p className='text-white barlow text-2xl'>628,000,000 km</p>
+                                        </div>
+
+                                        <div className='flex flex-col gap-2'>
+                                            <p className='text-[#A6ABCA] barlow text-xs tracking-widest'>EST. TRAVEL TIME</p>
+                                            <p className='text-white barlow text-2xl'>3 YEARS</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    )}
+
+
+                    {active === "TITAN" && (
+                        <>
+                            <div className='flex gap-20 justify-center items-center'>
+
+                                <div className='w-[35%]'>
+                                    <img className='w-full' src={Titan} alt={active} />
+                                </div>
+
+                                <div className='w-[40%]'>
+                           
+                                    <div className='flex flex-col gap-5'>
+                                        <h1 className="text-white text-8xl bellefair">TITAN</h1>
+                                        <p className="text-[#A6ABCA] barlow">The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.</p>
+                                    </div>
+
+                                    <div className='flex justify-between border-t border-slate-800 pt-6'>
+                                        <div className='flex flex-col gap-2'>
+                                            <p className='text-[#A6ABCA] barlow text-xs tracking-widest'>AVG. DISTANCE</p>
+                                            <p className='text-white barlow text-2xl'>1.6 BIL. km</p>
+                                        </div>
+
+                                        <div className='flex flex-col gap-2'>
+                                            <p className='text-[#A6ABCA] barlow text-xs tracking-widest'>EST. TRAVEL TIME</p>
+                                            <p className='text-white barlow text-2xl'>7 YEARS</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    )}
 
                 </div>
 
             </div>
 
         </div>
-    </>
-  )
+    )
 }
 
 export default DestinationA
--- src/pages/technologyA/index.tsx
+++ src/pages/technologyA/index.tsx
@@ -1,9 +1,19 @@
+import { useState } from 'react'
 import Technology from '../../assets/image-launch-vehicle-portrait.jpg'
+import Space from '../../assets/image-spaceport-portrait.jpg'
+import Capsule from '../../assets/image-space-capsule-portrait.jpg'
 import Nav from '../../components/nav'
+// import { Link } from 'react-router-dom'
 
 function TechnologyA() {
+    const [active, setActive] = useState(1);
+
+    const handleActive = (num:any) => {
+        setActive(num)
+    }
 
   return (
+    
 
     <>
         <div className="min-h-screen bg-[#101320] relative technology">
@@ -18,24 +28,67 @@ function TechnologyA() {
                 
 
                 
-                <div className=' flex justify-center items-center gap-7 w-[40vw]'>
-                    <ul className='flex flex-col gap-13'>
-                        <li><a className='bg-white rounded-full  px-6 py-5 ' href="/technologyA">1</a></li>
-                        <li><a className='bg-white rounded-full  px-6 py-5 ' href="/technologyB">2</a></li>
-                        <li><a className='bg-white rounded-full  px-6 py-5 ' href="/technologyC">3</a></li>
-                    </ul>
-                  
-                    <div className='flex flex-col gap-5'>
-                        
-                        <h1 className="text-white text-3xl bellefair">THE TERMINOLOGY… </h1>
-                        <p className="text-white text-5xl bellefair">LAUNCH VEHICLE</p>
-                        <p className="text-[#A6ABCA] barlow">A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!</p>
+                <div className=' flex justify-center items-center gap-7'>
+
+
+                    <div className='flex flex-col gap-7'>
+                        <button onClick={() => handleActive(1)} className='bg-white rounded-full  px-7 py-5 ' >1</button>
+                        <button onClick={() => handleActive(2)} className='bg-white rounded-full  px-7 py-5 '>2</button>
+                        <button onClick={() => handleActive(3)} className='bg-white rounded-full  px-7 py-5 ' >3</button>
                     </div>
+                    {active === 1 &&(
+                        <>
+                            <div className='flex flex-col gap-5 w-[30vw]'>
+                        
+                                <h1 className="text-white text-3xl bellefair">THE TERMINOLOGY… </h1>
+                                <p className="text-white text-5xl bellefair">LAUNCH VEHICLE</p>
+                                <p className="text-[#A6ABCA] barlow">A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!</p>
+                            </div>
 
-                </div>
+                    
 
-                <div className='w-[30vw]'>
-                    <img className='w-95' src={Technology} alt="" />
+                            <div className='w-[35vw]'>
+                                <img className='w-full' src={Technology} alt="" />
+                            </div>
+                        </>
+                    )}
+
+                    {active === 2 &&(
+                        <>
+                            <div className='flex flex-col gap-5 w-[30vw]'>
+                        
+                                <h1 className="text-white text-3xl bellefair">THE TERMINOLOGY </h1>
+                                <p className="text-white text-5xl bellefair">SPACEPORT</p>
+                                <p className="text-[#A6ABCA] barlow">A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.</p>
+                            </div>
+
+                    
+
+                            <div className='w-[35vw]'>
+                                <img className='w-full' src={Space} alt="" />
+                            </div>
+                        </>
+                    )}
+
+                    {active === 3 &&(
+                        <>
+                            <div className='flex flex-col gap-5 w-[30vw]'>
+                        
+                                <h1 className="text-white text-3xl bellefair">THE TERMINOLOGY… </h1>
+                                <p className="text-white text-5xl bellefair">SPACE CAPSULE</p>
+                                <p className="text-[#A6ABCA] barlow">A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.</p>
+                            </div>
+
+                    
+
+                            <div className='w-[35vw]'>
+                                <img className='w-full' src={Capsule} alt="" />
+                            </div>
+                        </>
+                    )}
+                  
+                    
+
                 </div>
 
             </div>
