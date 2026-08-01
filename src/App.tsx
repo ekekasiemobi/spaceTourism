be8554b294577e4e0db2de9c82ce1dfdec8d3ee6
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2026-08-01 01:08:41 +0100

    resolved deployment error in the app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import Home from "./pages/home"
 import DestinationA from './pages/destinationA'
-import DestinationB from './pages/destinationB'
-import DestinationC from './pages/destinationC'
-import DestinationD from './pages/destinationD'
+
 
 import CrewA from './pages/crewA'
 import CrewB from './pages/crewB'
 
 import TechnologyA from './pages/technologyA/'
-import TechnologyB from './pages/technologyB/'
-import TechnologyC from './pages/technologyC/'
+
 
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 function App() {
@@ -20,17 +17,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/destinationA" element={<DestinationA />}/>
-        <Route path="/destinationB" element={<DestinationB />}/>
-        <Route path="/destinationC" element={<DestinationC />}/>
-        <Route path="/destinationD" element={<DestinationD />}/>
 
         <Route path="/crewA" element={<CrewA />}/>
         <Route path="/crewB" element={<CrewB />}/>
 
         <Route path="/technologyA" element={<TechnologyA />}/>
-        <Route path="/technologyB" element={<TechnologyB />}/>
-         <Route path="/technologyC" element={<TechnologyC />}/>
-
 
       </Routes>
     </BrowserRouter>
